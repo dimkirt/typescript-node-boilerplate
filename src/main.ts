7a3d3a3a151c38b0d5cb921e7e98b95208cb321d
Dimitrios Kirtsios <dimkirts@gmail.com>
2023-04-01 22:25:06 +0200

    Create typescript template project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+
+const PORT = 3000;
+const app: Express = express();
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
