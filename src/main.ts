a1f9afe9c766b52bfe67b19c7282242d6ad017bd
Dimitrios Kirtsios <dimkirts@gmail.com>
2023-04-04 00:34:30 +0200

    Code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const PORT = 3000;
 const app: Express = express();
 
-app.get('/', (_req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
